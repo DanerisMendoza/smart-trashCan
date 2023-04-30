5fd52cc8fcabff931d188a7bc8c25666bfc6216f
Daneris <danerismendoza096@gmail.com>
2023-04-30 19:51:18 +0800

    the other component is malnfunction when using servo because it require a lot of power

--- arduino/Sensor/Sensor.ino
+++ arduino/Sensor/Sensor.ino
@@ -2,9 +2,11 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
+#include <Ultrasonic.h>
+#include <Servo.h>
 
 //note: set wifi to private because it turn off firewall
-String ipUrl = "192.168.1.6";
+String ipUrl = "192.168.1.5";
 String insertUrl = "http://"+ipUrl+"/smart-trashCan/php/insertData.php";
 String readUrl = "http://"+ipUrl+"/smart-trashCan/php/selectData.php";
 String postPin = "post=smarttrashcan";
@@ -14,11 +16,21 @@ StaticJsonDocument<200> doc;
 const byte trigger_pin = D0;
 const byte echo_pin = D1;
 long   pulseTime ;
-double distance; 
+
 //rgb
 byte red = D2;
 byte green = D3;
 byte blue = D4;
+//ultrasonic 2
+const byte trigger_pin2 = D5;
+const byte echo_pin2 = D6;
+long   pulseTime2;
+
+//servo
+Servo servo;
+
+Ultrasonic ultrasonic1(trigger_pin, echo_pin);
+Ultrasonic ultrasonic2(trigger_pin2, echo_pin2);
 
 void setup() {
   Serial.begin(9600);
@@ -30,24 +42,29 @@ void setup() {
       Serial.println("Connection failed");
   }
   //ULTRA SONIC DISTANCE SENSOR
-  pinMode (trigger_pin, OUTPUT); 
-  pinMode (echo_pin, INPUT);
+  // pinMode (trigger_pin, OUTPUT); 
+  // pinMode (echo_pin, INPUT);
   //rgb
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
- 
+   //ULTRA SONIC DISTANCE SENSOR 2
+  // pinMode (trigger_pin2, OUTPUT); 
+  // pinMode (echo_pin2, INPUT);
+  //servo
+  servo.attach(D7);
 }
 //note: make sure to not add space before and after &
 void loop() {
   String data = "";
-  readDataOfServer();
-  digitalWrite (trigger_pin, HIGH);
-  delayMicroseconds (10);
-  digitalWrite (trigger_pin, LOW);
-  pulseTime  = pulseIn(echo_pin, HIGH);
-  distance = double(pulseTime  * 0.034 / 2.0);
-  printStatus(distance);
+  // readDataOfServer();
+  float distance = ultrasonic1.distanceRead();
+  float distance2 = ultrasonic2.distanceRead();
+  
+  // Print the distances to the serial monitor
+  Serial.print("Distance from sensor 1: "+ (String) distance) + "CM";
+  Serial.print(" | ");
+  Serial.println("Distance from sensor 2: " + (String) distance2 + "CM");
 
   if(distance <= 5){
     data = "post=smarttrashcan&data=3";
@@ -71,12 +88,17 @@ void loop() {
     digitalWrite(blue, LOW);
   }
   sendDataToServer(data);
-}
-
-void printStatus(double distance){
-  Serial.print ("Distance= ");              
-  Serial.print (distance);   
-  Serial.println("cm");
+  delay(50); 
+  //note: the servo bug trigger because it require more power therefore the other components is malfunctioning
+  if(distance2 <= 10){
+    Serial.println("Servo rotate to 90 deg");
+    servo.write(180);
+    delay(2000);
+  }
+  else if(distance2 > 10){
+    servo.write(0);
+  }
+  delay(50); 
 }
 
 void sendDataToServer(String data) {
